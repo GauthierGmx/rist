78f94e0377821ef9704a7f4df9fa084c645daf16
Gabriel Vernis <119617448+JPDubuisson@users.noreply.github.com>
2023-11-13 15:38:08 +0100

    Update listeActivite.py
    ajout de jeux vidéos dans l'ensemble loisir

--- developpement/categorie/listeActivite.py
+++ developpement/categorie/listeActivite.py
@@ -5,9 +5,9 @@ Created on Fri Sep 29 14:25:01 2023
 @author: ggoumeaux001
 """
 
-Art = ["Peinture","Dessin","Photographie","Bande dessinée","Poésie","Théâtre","Cirque","Cinéma","Jeux Vidéo"]
+Art = ["Peinture","Dessin","Photographie","Bande dessinée","Poésie","Théâtre","Cirque","Cinéma"]
 Danse = ["Hip Hop","Breakdance","Jazz","Bachata","Disco","Pole Dance","Rock","Salsa","Slow","Zumba"]
-Loisir = ["Pêche","Randonnée","Jeux de société","Jeu de piste","Escape Game","Accrobranche","Quizz","Lancer de hache","Laser game","Karaoké"]
+Loisir = ["Pêche","Randonnée","Jeux de société","Jeu de piste","Escape Game","Accrobranche","Quizz","Lancer de hache","Laser game","Karaoké","Jeux Vidéo"]
 Sortie = ["Soirée","Festival","Convention","Concert","Bar","Apéro","Repas","Pique-nique","Promenade","Visite"]
 Voyage = ["Voyage en liberté","Voyage guidé","Voyage itinérant","Séjour à l'hotel","Voyage semi-itinérant","Tour du monde","Croisière","Trek","Road Trip","Camping","Auto-Stop","Covoiturage"]
 
