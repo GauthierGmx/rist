9532692e990b303cddf8e39e3cef81e044cee9c1
Solène Martin <smartin016@iutbayonne.univ-pau.fr>
2023-12-22 10:49:19 +0100

    Création des classes classUtilisateur.php et classActivite.php + Modification avec les get&set

--- developpement/recommandations.php
+++ developpement/recommandations.php
@@ -33,24 +33,9 @@
 </head>
 <body>
 <?php
-
-class Utilisateur {
-    // Attributs
-    public $pseudonyme; // String
-    public $nbActivite; // Int
-    public $coordGPS;   // Array : [0] X ; [1] Y
-    public $budget;     // Int arrondi à 2 chiffres après la virgule
-    public $categories; // Array : [0] catégorie 1 ; [1] catégorie 2 ; [2] catégorie 3
-
-    // Constructeur
-    public function __construct($pseudonyme,$nbActivite,$coordGPS,$budget,$categories){
-        $this->pseudonyme = $pseudonyme;
-        $this->nbActivite = $nbActivite;
-        $this->coordGPS = $coordGPS;
-        $this->budget = $budget;
-        $this->categories = $categories;
-    }
-}
+#INCLUDE DES CLASSES
+include 'classUtilisateur.php';
+include 'classActivite.php';
 
 // Récupérez la chaîne sérialisée à partir de la requête GET
 $serializedUtilisateur = urldecode($_GET['utilisateur']);
@@ -60,15 +45,15 @@ $utilisateur = unserialize($serializedUtilisateur);
 
 // Affichez les informations de l'utilisateur
 echo "<h3>Informations de l'utilisateur</h3>";
-echo "<p>Pseudonyme : " . $utilisateur->pseudonyme . "</p>";
-echo "<p>Nombre d'activités : " . $utilisateur->nbActivite . "</p>";
-echo "<p>Coordonnées GPS x : " . $utilisateur->coordGPS[0] . "</p>";
-echo "<p>Coordonnées GPS y : " . $utilisateur->coordGPS[1] . "</p>";
-echo "<p>Budget : " . $utilisateur->budget . "</p>";
+echo "<p>Pseudonyme : " . $utilisateur->getPseudonyme() . "</p>";
+echo "<p>Nombre d'activités : " . $utilisateur->getNbActivite() . "</p>";
+echo "<p>Coordonnées GPS x : " . $utilisateur->getCoordGPS()[0] . "</p>";
+echo "<p>Coordonnées GPS y : " . $utilisateur->getCoordGPS()[1] . "</p>";
+echo "<p>Budget : " . $utilisateur->getBudget() . "</p>";
 
 echo "<p>Categories préférées :</p>";
 echo "<ul>";
-foreach ($utilisateur->categories as $category) {
+foreach ($utilisateur->getCategories() as $category) {
     echo "<li>$category</li>";
 }
 echo "</ul>";
