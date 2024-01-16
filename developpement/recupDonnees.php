2a56add7126764993fb56ba88061e782e0be43d2
Gabriel Vernis <gvernis@iutbayonne.univ-pau.fr>
2024-01-16 14:54:49 +0100

    Nettoyage de master

--- developpement/recupDonnees.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $prctGeographie = $_POST['prctGeographie'];
-    $prctPrix = $_POST['prctPrix'];
-    $prctCategories = $_POST['prctCategories'];
-    echo "<p>$prctGeographie $prctPrix $prctCategories</p>"
-?>
