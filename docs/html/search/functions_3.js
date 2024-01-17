4da209d4a25405de404c68abf1dab06976aeb43d
Julien Loridant <jloridant@iutbayonne.univ-pau.fr>
2024-01-17 15:39:15 +0100

    changement du répertoire de la documentation

---
+++ docs/html/search/functions_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['haversinedistance_0',['haversineDistance',['../fonctions_8php.html#a87f98f73cc5748a2acc13a3ae6348f14',1,'fonctions.php']]]
+];
---
+++ docs/html/search/functions_6.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['tostring_0',['toString',['../class_activite.html#a5558c5d549f41597377fa1ea8a1cefa3',1,'Activite']]],
+  ['tri_1',['tri',['../fonctions_8php.html#ab5b83cfbf0e45e8b281e279fea52ff84',1,'fonctions.php']]]
+];
